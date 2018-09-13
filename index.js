164bb1306ade54063cfa05cc1c2aaab3da01919d
Glitch (potena-bot) <none>
2018-09-13 17:09:10 +0000

    🍂📍 Updated with Glitch

--- index.js
+++ index.js
@@ -9,17 +9,52 @@ if (!token) {
 
 const bot = new Botgram(token);
 
+bot.command("help", function (msg, reply, next) {
+  reply.markdown("Aee! T l sev ritt");
+});
+
+bot.command("mamma", function (msg, reply, next) {
+  reply.markdown("Ngul a mammt!");
+});
+   
+bot.command("foto", function (msg, reply, next) {
+  reply.photo("https://mas-group.inf.h-brs.de/wp-content/uploads/2015/06/Daniele_Nardi-292x262.jpg");
+});
+
+/* TO ADD LIST
+
+ - essi vi
+ - frssora
+ - random pick of photo
+ - random curses in molisn't language
+*/
+
+
+
 function onMessage(msg, reply) {
 	to_reply = msg.text
-	if (msg.text.indexOf('pollo') > -1) {
+	if (msg.text.indexOf('pollo') > -1 || msg.text.indexOf('Pollo') > -1) {
 		to_reply = 'Forse vulevi dire Pllashtr!'
     reply.text(to_reply).then();
     return;   
-  } else if (msg.text.indexOf('tarallo') > -1) {
+  } else if (msg.text.indexOf('polli' || msg.text.indexOf('Polli') > -1) > -1) {
+		to_reply = 'Forse vulevi dire Pllashtr!'
+    reply.text(to_reply).then();
+    return;       
+  } else if (msg.text.indexOf('tarallo') > -1  || msg.text.indexOf('Tarallo') > -1) {
+		to_reply = 'Vscuott!'
+    reply.text(to_reply).then();
+    return;       
+  } else if (msg.text.indexOf('taralli' ) > -1  || msg.text.indexOf('Taralli') > -1) {
 		to_reply = 'Vscuotter!'
     reply.text(to_reply).then();
     return;       
-  }
+  } else if (msg.text.indexOf('alberto') > -1 || msg.text.indexOf('Alberto') > -1) {
+		to_reply = 'Albrdo!'
+    reply.text(to_reply).then();
+    return;       
+  } 
+  
 }
 
 bot.text(onMessage);
