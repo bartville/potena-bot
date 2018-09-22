d7caa255e6e0df2f9e6697b7bc595cbda69d5ce2
Glitch (potena-bot) <none>
2018-09-22 11:30:20 +0000

    adding gif reply

--- padulo.js
+++ padulo.js
@@ -2,10 +2,10 @@ var utils = require("./utils.js");
 var people = require("./persone.js");
 
 exports.times = ['oggi', 'domani', "quest'anno", "l'anno prossimo", "questo semestre", "per i prossimi 3 anni"];
-exports.verbs = ['si occupa di', 'presenta una demo a', "e' responsabile di", "fa lezione a"];
+exports.verbs = ['si occupa di', 'presenta una demo a', "é responsabile di", "fa lezione a"];
 exports.duties = ['openDIAG', 'Maker Faire', 'TDP', 'Laboratorio di IA', 'OberSeminar', "Lista Acquisti", "RoboCup"];
 
-exports.datasets = ["catacombe di Priscilla", "Jesi", "Ancona", "Caffarella", "Eschikon", "Zurigo"];
+exports.datasets = ["catacombe di Priscilla", "Jesi", "Ancona", "Caffarella", "Eschikon", "Zurigo", "Cambuash"];
 
 exports.meeting_themes = ["le normative sulla sicurezza del laboratorio", 
                           "la classificazione della peronospera per i girasoli", 
@@ -30,7 +30,7 @@ exports.basePadulo = function() {
 exports.labMeetingPadulo = function() {
   var time = "al prossimo meeting di laboratorio";
   var subject = utils.randomElementFromList(people.subjects);
-  var verb = "presentera'";
+  var verb = "presenterà";
   var meeting_theme = utils.randomElementFromList(this.meeting_themes);
   var sentence = '"' + time + ' ' + subject + ' ' + verb + ' ' + meeting_theme + '"';
   return sentence;  
@@ -39,7 +39,7 @@ exports.labMeetingPadulo = function() {
 exports.datasetAcquisitionPadulo = function() {
   var time = utils.randomElementFromList(this.times);
   var subject = utils.randomElementFromList(people.subjects);
-  var verb = "andra' ad acquisire un dataset a";
+  var verb = "andrà ad acquisire un dataset a";
   var dataset = utils.randomElementFromList(this.datasets);
   var sentence = '"' + time + ' ' + subject + ' ' + verb + ' ' + dataset + '"';
   return sentence;     
